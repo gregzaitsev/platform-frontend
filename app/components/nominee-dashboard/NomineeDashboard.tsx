6ed9ed1d458054b4c45c008321269197c115161e
lexey <tblz@palavara.com>
2019-07-18 14:43:27 +0200

    add basic steps data, basic step component

--- app/components/nominee-dashboard/NomineeDashboard.tsx
+++ app/components/nominee-dashboard/NomineeDashboard.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { compose,nest } from "recompose";
+import { branch, compose, nest, renderComponent, withProps } from "recompose";
 import { FormattedMessage } from "react-intl-phraseapp";
 
 import { withContainer } from "../../utils/withContainer.unsafe";
@@ -11,26 +11,46 @@ import { selectNomineeKycRequestStatus } from "../../modules/kyc/selectors";
 import { ERequestStatus } from "../../lib/api/KycApi.interfaces";
 import { Panel } from "../shared/Panel";
 
-const NomineeDashboardLayout = ({
-  emailVerified,
-  backupCodesVerified,
-  kycRequestStatus,
-  verificationIsComplete
-}) => {
-  if(!emailVerified) {
-    return <>email</>
-  } else if(backupCodesVerified){
-    return <>backupcode</>
-  } else if (kycRequestStatus === ERequestStatus.DRAFT){
-    return <>kyc</>
-  } else if (kycRequestStatus === ERequestStatus.PENDING){
-    return <>wait for confirmation</>
-  } else if(verificationIsComplete) {
-    return <>tasks </>
-  }
-}
 
-const AccountSetupContainer = ({children}) => <div data-test-id="nominee-dashboard">
+const AccountSetupStep = ({stepData}) => {
+  return <div className="acocunt-steup-step-wrapper">
+    <div className="step-ticker">{stepData.condition === true ? "x":"0"}</div>
+    <div className="step-title">{stepData.title}</div>
+    <div className="step-component" >{stepData.component}</div>
+  </div>
+};
+
+const NomineeDashboardLayout = ({
+  accountSetupStepsData
+}) => {
+  return accountSetupStepsData.map((stepData) => {
+    return <AccountSetupStep stepData={stepData} />;
+  })
+};
+
+const nomineeAccountSetupSteps = (emailVerified, backupCodesVerified, kycRequestStatusIsDraft) => [
+  {
+    key: 'verifyEmail',
+    conditionCompleted: emailVerified,
+    title: <FormattedMessage id="account-setup.verify-email" />,
+    component: <>email</>
+  },
+  {
+    key: 'verifyBackupCodes',
+    conditionCompleted: emailVerified,
+    title: <FormattedMessage id="account-setup.verify-backup-codes" />,
+    component: <>backup codes</>
+  },
+  {
+    key: 'startKyc',
+    conditionCompleted: kycRequestStatusIsDraft,
+    title: <FormattedMessage id="account-setup.verify-your-company" />,
+    component: <>start kyc</>
+  }
+];
+
+
+const AccountSetupContainer = ({ children }) => <div data-test-id="nominee-dashboard">
   <h1>
     <FormattedMessage id="account-setup.welcome-to-neufund" />
   </h1>
@@ -38,12 +58,13 @@ const AccountSetupContainer = ({children}) => <div data-test-id="nominee-dashboa
     <FormattedMessage id="account-setup.please-complete-setup" />
   </p>
   <Panel>
-  {children}
+    {children}
   </Panel>
 </div>;
 
 
 export const NomineeDashboard = compose(
+  withContainer(nest(Layout, AccountSetupContainer)),
   appConnect({
     stateToProps: state => ({
       emailVerified: selectIsUserEmailVerified(state.auth),
@@ -52,7 +73,13 @@ export const NomineeDashboard = compose(
       verificationIsComplete: SelectIsVerificationFullyDone(state),
     })
   }),
-  withContainer(nest(Layout,AccountSetupContainer))
+  branch(props => props.kycRequestStatus === ERequestStatus.PENDING, renderComponent(() => <>kyc pending</>)),
+  branch(props => props.verificationIsComplete, renderComponent(() => <>no tasks for you today</>)),
+  withProps(({emailVerified, backupCodesVerified, kycRequestStatusIsDraft}) => {
+    return ({
+      accountSetupStepsData: nomineeAccountSetupSteps(emailVerified, backupCodesVerified, kycRequestStatusIsDraft)
+    })
+  }),
 )(NomineeDashboardLayout);
 
 
