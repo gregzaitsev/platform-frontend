87e12a85ae20df1873a832a89309546cf3031a2e
lexey <tblz@palavara.com>
2019-07-18 17:05:12 +0200

    add some basic css

--- app/components/nominee-dashboard/NomineeDashboard.tsx
+++ app/components/nominee-dashboard/NomineeDashboard.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { branch, compose, nest, renderComponent, withProps } from "recompose";
 import { FormattedMessage } from "react-intl-phraseapp";
+import * as cn from 'classnames'
 
 import { withContainer } from "../../utils/withContainer.unsafe";
 import { Layout } from "../layouts/Layout";
@@ -12,6 +13,8 @@ import { ERequestStatus } from "../../lib/api/KycApi.interfaces";
 import { Panel } from "../shared/Panel";
 import { IAccountSetupStepData, IStepComponentProps, prepareSetupAccountSteps } from "./utils";
 
+import * as styles from './NomineeDashboard.module.scss'
+
 interface INomineeAccountSetupSteps {
   accountSetupStepsData: IStepComponentProps[];
 }
@@ -21,8 +24,8 @@ interface IStepTickerProps {
   active: boolean,
 }
 
-const StepTicker:React.FunctionComponent<IStepTickerProps> = ({done, active}) => {
-  if(done){
+const StepTicker: React.FunctionComponent<IStepTickerProps> = ({ done, active }) => {
+  if (done) {
     return <>x</>
   } else if (active) {
     return <>v</>
@@ -31,21 +34,29 @@ const StepTicker:React.FunctionComponent<IStepTickerProps> = ({done, active}) =>
   }
 };
 
-class AccountSetupStep extends React.Component<IStepComponentProps,{isOpen: boolean}> {
+class AccountSetupStep extends React.Component<IStepComponentProps, { isOpen: boolean }> {
   state = {
     isOpen: this.props.isActive
   };
 
-  toggleOpen = () => {};
+  toggleOpen = () => {
+    this.setState(s => ({ isOpen: !s.isOpen }))
+  };
 
   render() {
-    const { done,isActive, title, component } = this.props;
-    return <div className="acocunt-steup-step-wrapper">
-      <div className="step-ticker">
-        <StepTicker done={done} active={isActive}/>
+    const { done, isActive, title, component } = this.props;
+    return <div className={styles.accountSetupStepWrapper}>
+      <div className={styles.ticker}>
+        <StepTicker done={done} active={isActive} />
       </div>
-      <div className="step-title">{title}</div>
-      <div className={this.state.isOpen ? "open" : "closed"}>{component}</div>
+      <div
+        className={styles.title}
+        onClick={(this.props.done || this.props.isActive) ? this.toggleOpen : undefined}
+      >
+        {title}
+      </div>
+      <span className={styles.line} />
+      <div className={cn(styles.component, { [styles.open]: this.state.isOpen })}>{component}</div>
     </div>
   }
 }
@@ -108,7 +119,7 @@ export const NomineeDashboard = compose<INomineeAccountSetupSteps, {}>(
   }),
   branch<any>(props => props.kycRequestStatus === ERequestStatus.PENDING, renderComponent(() => <>kyc pending</>)),
   branch<any>(props => props.verificationIsComplete, renderComponent(() => <>no tasks for you today</>)),
-  withProps<any,any>(({ emailVerified, backupCodesVerified, kycRequestStatus }:any) => {
+  withProps<any, any>(({ emailVerified, backupCodesVerified, kycRequestStatus }: any) => {
     return ({
       accountSetupStepsData: prepareSetupAccountSteps(
         nomineeAccountSetupSteps(emailVerified, backupCodesVerified, kycRequestStatus !== ERequestStatus.DRAFT)
