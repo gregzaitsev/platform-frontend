91133d16049f360850542cff6881c8d41c650b44
lexey <tblz@palavara.com>
2019-07-22 17:04:21 +0200

    extract accountSetup backupCodesWidget and it's css to separate files

---
+++ app/components/settings/backup-seed/AccountSetupBackupSeedWidget.tsx
@@ -0,0 +1,57 @@
+import * as React from "react";
+import { FormattedMessage } from "react-intl-phraseapp";
+import { TTranslatedString } from "../../../types";
+
+import { Button, EButtonLayout, EButtonTheme } from "../../shared/buttons/Button";
+
+import * as styles from "./AccountSetupBackupSeedWidget.module.scss";
+
+interface IStateProps {
+  backupCodesVerified: boolean;
+}
+
+interface IDispatchProps {
+  startBackupProcess: () => void
+}
+
+interface IWidgetBaseProps {
+  widgetDataTestId:string,
+  widgetText:TTranslatedString,
+  buttonDataTestId: string,
+  buttonText:TTranslatedString,
+}
+
+const BackupCodesWidgetBase:React.FunctionComponent<IWidgetBaseProps & IDispatchProps> = ({ startBackupProcess, widgetDataTestId, widgetText, buttonDataTestId, buttonText }) =>
+  <section
+    data-test-id={widgetDataTestId}
+    className={styles.accountSetupSection}
+  >
+    <p className={styles.accountSetupText}>
+      {widgetText}
+    </p>
+    <Button
+      onClick={startBackupProcess}
+      theme={EButtonTheme.BRAND}
+      layout={EButtonLayout.PRIMARY}
+      data-test-id={buttonDataTestId}
+    >
+      {buttonText}
+    </Button>
+  </section>
+
+export const AccountSetupBackupWidgetLayout: React.FunctionComponent<IStateProps & IDispatchProps> = ({ backupCodesVerified, startBackupProcess }) =>
+  backupCodesVerified
+    ? <BackupCodesWidgetBase
+      widgetDataTestId="backup-seed-verified-section"
+      widgetText={<FormattedMessage id="settings.backup-seed-widget.backed-up-seed"/>}
+      buttonDataTestId="backup-seed-verified-section.view-again"
+      buttonText={<FormattedMessage id="settings.backup-seed-widget.view-again"/>}
+      startBackupProcess={startBackupProcess}
+    />
+    : <BackupCodesWidgetBase
+      widgetDataTestId="backup-seed-unverified-section"
+      widgetText={<FormattedMessage id="settings.backup-seed-widget.write-down-recovery-phrase"/>}
+      buttonDataTestId="backup-seed-widget-link-button"
+      buttonText={<FormattedMessage id="settings.backup-seed-widget.backup-phrase"/>}
+      startBackupProcess={startBackupProcess}
+    />;
--- app/components/settings/backup-seed/BackupSeedWidget.tsx
+++ app/components/settings/backup-seed/BackupSeedWidget.tsx
@@ -1,3 +1,4 @@
+import * as cn from "classnames";
 import * as React from "react";
 import { FormattedMessage } from "react-intl-phraseapp";
 import { compose } from "recompose";
@@ -10,9 +11,11 @@ import { actions } from "../../../modules/actions";
 import { selectBackupCodesVerified } from "../../../modules/auth/selectors";
 import { appConnect } from "../../../store";
 import { EColumnSpan } from "../../layouts/Container";
-import { Button, ButtonLink, EButtonLayout, EButtonTheme, EIconPosition } from "../../shared/buttons/index";
+import { ButtonLink, EButtonLayout, EIconPosition } from "../../shared/buttons/index";
 import { Panel } from "../../shared/Panel";
 import { profileRoutes } from "../routes";
+import { AccountSetupBackupWidgetLayout } from "./AccountSetupBackupSeedWidget";
+
 import * as styles from "./BackupSeedWidget.module.scss";
 
 interface IStateProps {
@@ -28,83 +31,6 @@ interface IExternalProps {
   step: number;
 }
 
-const AccountSetupBackupWidgetLayout: React.FunctionComponent<IStateProps & IDispatchProps> = ({ backupCodesVerified, startBackupProcess }) =>
-  backupCodesVerified ? (
-    <section
-      data-test-id="backup-seed-verified-section"
-      className={styles.accountSetupSection}
-    >
-      <p className={styles.accountSetupText}>
-        <FormattedMessage id="settings.backup-seed-widget.backed-up-seed" />
-      </p>
-      <Button
-        onClick={startBackupProcess}
-        theme={EButtonTheme.BRAND}
-        layout={EButtonLayout.PRIMARY}
-        data-test-id="backup-seed-verified-section.view-again"
-      >
-        <FormattedMessage id="settings.backup-seed-widget.view-again" />
-      </Button>
-    </section>
-  ) : (
-    <section
-      data-test-id="backup-seed-unverified-section"
-      className={styles.accountSetupSection}
-    >
-      <p className={styles.accountSetupText}>
-        <FormattedMessage id="settings.backup-seed-widget.write-down-recovery-phrase" />
-      </p>
-      <Button
-        onClick={startBackupProcess}
-        theme={EButtonTheme.BRAND}
-        layout={EButtonLayout.PRIMARY}
-        data-test-id="backup-seed-widget-link-button"
-      >
-        <FormattedMessage id="settings.backup-seed-widget.backup-phrase" />
-      </Button>
-    </section>
-  );
-
-
-const BackupSeedWidgetLayout: React.FunctionComponent<IStateProps> = ({ backupCodesVerified }) =>
-  backupCodesVerified ? (
-    <section
-      data-test-id="backup-seed-verified-section"
-      className={styles.section}
-    >
-      <p className={styles.text}>
-        <FormattedMessage id="settings.backup-seed-widget.backed-up-seed" />
-      </p>
-      <ButtonLink
-        to={profileRoutes.seedBackup}
-        layout={EButtonLayout.SECONDARY}
-        iconPosition={EIconPosition.ICON_AFTER}
-        svgIcon={arrowRight}
-        data-test-id="backup-seed-verified-section.view-again"
-      >
-        <FormattedMessage id="settings.backup-seed-widget.view-again" />
-      </ButtonLink>
-    </section>
-  ) : (
-    <section
-      data-test-id="backup-seed-unverified-section"
-      className={styles.section}
-    >
-      <p className={styles.text}>
-        <FormattedMessage id="settings.backup-seed-widget.write-down-recovery-phrase" />
-      </p>
-      <ButtonLink
-        to={profileRoutes.seedBackup}
-        data-test-id="backup-seed-widget-link-button"
-        layout={EButtonLayout.SECONDARY}
-        iconPosition={EIconPosition.ICON_AFTER}
-        svgIcon={arrowRight}
-      >
-        <FormattedMessage id="settings.backup-seed-widget.backup-phrase" />
-      </ButtonLink>
-    </section>
-  );
-
 const BackupSeedWidgetBase: React.FunctionComponent<IStateProps & IExternalProps> = ({
   backupCodesVerified,
   columnSpan,
@@ -114,14 +40,50 @@ const BackupSeedWidgetBase: React.FunctionComponent<IStateProps & IExternalProps
     headerText={<FormattedMessage id="settings.backup-seed-widget.header" />}
     rightComponent={
       backupCodesVerified ? (
-        <img src={successIcon} className={styles.icon} aria-hidden="true" alt="" />
+        <img src={successIcon} className={styles.icon} aria-hidden="true" />
       ) : (
-        <img src={warningIcon} className={styles.icon} aria-hidden="true" alt="" />
+        <img src={warningIcon} className={styles.icon} aria-hidden="true" />
       )
     }
     data-test-id="profile.backup-seed-widget"
   >
-    <BackupSeedWidgetLayout backupCodesVerified={backupCodesVerified} />
+    {backupCodesVerified ? (
+      <section
+        data-test-id="backup-seed-verified-section"
+        className={cn(styles.section, "d-flex flex-wrap align-content-around")}
+      >
+        <p className={cn(styles.text, "pt-2")}>
+          <FormattedMessage id="settings.backup-seed-widget.backed-up-seed" />
+        </p>
+        <ButtonLink
+          to={profileRoutes.seedBackup}
+          layout={EButtonLayout.SECONDARY}
+          iconPosition={EIconPosition.ICON_AFTER}
+          svgIcon={arrowRight}
+          data-test-id="backup-seed-verified-section.view-again"
+        >
+          <FormattedMessage id="settings.backup-seed-widget.view-again" />
+        </ButtonLink>
+      </section>
+    ) : (
+      <section
+        data-test-id="backup-seed-unverified-section"
+        className={cn(styles.section, "d-flex flex-wrap align-content-around")}
+      >
+        <p className={cn(styles.text, "pt-2")}>
+          <FormattedMessage id="settings.backup-seed-widget.write-down-recovery-phrase" />
+        </p>
+        <ButtonLink
+          to={profileRoutes.seedBackup}
+          data-test-id="backup-seed-widget-link-button"
+          layout={EButtonLayout.SECONDARY}
+          iconPosition={EIconPosition.ICON_AFTER}
+          svgIcon={arrowRight}
+        >
+          <FormattedMessage id="settings.backup-seed-widget.backup-phrase" />
+        </ButtonLink>
+      </section>
+    )}
   </Panel>
 );
 
