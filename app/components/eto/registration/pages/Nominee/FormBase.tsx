396100778cf26c85d1c3da547eded7502c115828
lexey <tblz@palavara.com>
2019-08-02 17:43:41 +0200

    fix nominee form base

--- app/components/eto/registration/pages/Nominee/FormBase.tsx
+++ app/components/eto/registration/pages/Nominee/FormBase.tsx
@@ -1,15 +1,14 @@
 import * as React from "react";
-import { Form } from "../../../../shared/forms/Form";
 import { FormattedMessage } from "react-intl-phraseapp";
 
 import * as styles from "./Nominee.module.scss";
 
 const FormBase: React.FunctionComponent = ({ children }) =>
-  <Form className={styles.form}>
+  <div className={styles.form}>
     <h4 className={styles.header}>
       <FormattedMessage id="eto.form.eto-nominee.title" />
     </h4>
     {children}
-  </Form>;
+  </div>;
 
   export {FormBase}
