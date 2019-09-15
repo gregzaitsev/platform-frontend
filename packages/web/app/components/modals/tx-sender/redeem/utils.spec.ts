24481882669039ff00e9114519b6925661888854
Drakkein <wiktor@furman.me>
2019-09-15 20:30:44 +0200

    Fix getPossibleMaxUlps util function

--- packages/web/app/components/modals/tx-sender/redeem/utils.spec.ts
+++ packages/web/app/components/modals/tx-sender/redeem/utils.spec.ts
@@ -4,8 +4,8 @@ import { convertToBigInt } from "../../../../utils/Number.utils";
 import { getPossibleMaxUlps } from "./utils";
 
 describe("getPossibleMaxUlps", () => {
-  it("should correctly quantize value", () => {
-    expect(getPossibleMaxUlps(convertToBigInt("10278127.1988124"), "10278127.1988")).to.be.eq(
+  it("should correctly select value", () => {
+    expect(getPossibleMaxUlps(convertToBigInt("10278127.1988124"), "10278127.19")).to.be.eq(
       convertToBigInt("10278127.1988124"),
     );
     expect(getPossibleMaxUlps(convertToBigInt("10278127.1988124"), "124.28")).to.be.eq(
--- packages/web/app/components/modals/tx-sender/redeem/utils.ts
+++ packages/web/app/components/modals/tx-sender/redeem/utils.ts
@@ -1,17 +1,18 @@
-import BigNumber from "bignumber.js";
-
-import { BANKING_AMOUNT_SCALE, MONEY_DECIMALS } from "../../../../config/constants";
+import { BANKING_AMOUNT_SCALE } from "../../../../config/constants";
 import { compareBigNumbers } from "../../../../utils/BigNumberUtils";
 import { convertToBigInt } from "../../../../utils/Number.utils";
+import { ERoundingMode, toFixedPrecision } from "../../../shared/formatters/utils";
 
 export const getPossibleMaxUlps = (
   maxUlpsValue: string,
   providedDecimal: number | string,
   decimalPrecision = BANKING_AMOUNT_SCALE,
 ) => {
-  const nEURBalance = new BigNumber(maxUlpsValue)
-    .div(new BigNumber(10).pow(MONEY_DECIMALS))
-    .toFixed(decimalPrecision, BigNumber.ROUND_DOWN);
+  const nEURBalance = toFixedPrecision({
+    value: maxUlpsValue,
+    decimalPlaces: decimalPrecision,
+    roundingMode: ERoundingMode.DOWN,
+  });
 
   // Whole precision number should be passed when there is whole balance redeemed
   // also when user provided value has been used, then it have to be converted to Q18 via convertToBigInt
